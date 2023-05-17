569f1efddf801174eea716fdd1139789c4211235
Júlio Juriolli <juliocesarjuriolli@gmail.com>
2023-05-16 23:03:46 -0300

    feat: launching python script and node on electron application

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const net = require('net');
+
 const path = require('path')
 console.log(__dirname)
 const createWindow = () => {
@@ -9,6 +11,14 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+
+  const client = net.connect(50000, '127.0.0.1', () => {
+    console.log('Connected to server');
+  });
+
+  client.on('data', (data) => {
+    console.log(data.toString())
+  });
 
   win.loadFile(path.join(__dirname, 'dist', 'index.html'))
 }
