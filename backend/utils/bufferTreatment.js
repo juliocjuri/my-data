cd711535db906f7840bbeee503c0a563f37fe4c4
Júlio Juriolli <juliocesarjuriolli@gmail.com>
2023-05-18 08:42:30 -0300

    fixes: modularized some functions to another file and fixed some minor things

---
+++ backend/utils/bufferTreatment.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+function getJsonFromAnalyzer(data){
+    const bufferArray = data.toJSON().data;
+
+    //98 is the ASCII code for the letter b (the data object starts with the letter b)
+    const arrayFirstIndex = bufferArray.indexOf(98);
+    
+    //39 is index of single comma (last index of the json we want)
+    const arrayLastIndex = bufferArray.lastIndexOf(39)
+    
+    const dataArray = bufferArray.slice(arrayFirstIndex + 2, arrayLastIndex);
+    const stringFromBuffer = String.fromCharCode.apply(null, dataArray);
+    const jsonFromString = JSON.parse(stringFromBuffer);
+
+    console.log('jsonfromstring <<')
+    console.log(jsonFromString)
+    return jsonFromString;
+}
+
+function addAnotherJsonInput(filePath, json){
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        let objectWithBracketsRemoved = data.replace(']', ',');
+        console.log("1. Reading")
+
+        fs.writeFile(filePath, objectWithBracketsRemoved, 'utf8', function (err) {
+          console.log("2. Exchanging")
+          if (err) return console.log(err);
+          fs.appendFile(filePath, `${json}]\n`, function (err) {
+            if (err) {
+              return console.log(err);
+            }else{
+                console.log("3. New info");
+                return true
+            }
+          });
+        });
+      });
+}
+
+module.exports ={
+    getJsonFromAnalyzer,
+    addAnotherJsonInput
+}
--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const net = require('net');
 const cp = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const bufferTreatment = require('./backend/utils/bufferTreatment')
 
-const pythonScriptPath = path.join(__dirname, 'src', 'connection')
+const unpolishedDataPath = path.join(__dirname, 'volatile', 'unpolishedData.json');
+const trafficAnalyzerScriptPath = path.join(__dirname, 'src', 'connection')
 
-cp.exec(`cd ${pythonScriptPath} && pm2 start traffic_analyzer.py`)
+cp.exec(`cd ${trafficAnalyzerScriptPath} && pm2 start traffic_analyzer.py`)
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -22,88 +24,57 @@ const createWindow = () => {
     const client = net.connect(50000, '127.0.0.1', () => {
       console.log('Connected to server');
     });
-  
+
     client.on('data', (data) => {
-      const bufferArray = data.toJSON().data;
-      
-      //98 is the ASCII code for the letter b (the data object starts with the letter b)
-      const arrayFirstIndex = bufferArray.indexOf(98);
+      const json = JSON.stringify(bufferTreatment.getJsonFromAnalyzer(data));
+      const result = bufferTreatment.addAnotherJsonInput(unpolishedDataPath, json)
+      console.log('resultado >>>>')
+      console.log(result)
 
-      //39 is index of single comma (last index of the json we want)
-      const arrayLastIndex =  bufferArray.lastIndexOf(39)
-  
-      const dataArray = bufferArray.slice(arrayFirstIndex + 2, arrayLastIndex); 
-      const stringFromBuffer = String.fromCharCode.apply(null, dataArray);
-  
-      const jsonFromString = JSON.parse(stringFromBuffer);
+      let rawdata = fs.readFileSync(unpolishedDataPath, () => { console.log('leu') })
 
-      const unpolishedDataPath = path.join(__dirname, 'volatile', 'unpolishedData.json');
-      
-      fs.readFile(unpolishedDataPath, 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        var result = data.replace(']', ',');
-        console.log("Reading")
+      console.log('raw >>>' + rawdata)
 
-        fs.writeFile(unpolishedDataPath, result, 'utf8', function (err) {
-          console.log("Exchanging")
-          if (err) return console.log(err);
-          fs.appendFile(unpolishedDataPath, `${stringFromBuffer}]\n`, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("New info");
-        }); 
-        });
-      });
+      let person = JSON.parse(rawdata)
+      const lastRegister = person[person.length - 1]
 
-      console.log(jsonFromString)
+      console.log(lastRegister)
 
-let rawdata = fs.readFileSync(unpolishedDataPath, () => { console.log('leu')})
+      const applications = Object.values(lastRegister)
+      console.log(applications.length)
 
-console.log('raw >>>' + rawdata)
+      const values = [];
+      for (let i = 0; i < applications.length; i++) {
+        console.log('Application >>>>>>')
+        console.log(applications[i])
 
-let person = JSON.parse(rawdata)
-const lastRegister = person[person.length - 1]
 
-console.log(lastRegister)
+        let download = applications[i].download.slice(0, applications[i].download.indexOf('.') + 2);
 
-const applications = Object.values(lastRegister)
-console.log(applications.length)
 
-const values = [];
-for(let i = 0; i < applications.length; i++){
-    console.log('Application >>>>>>')
-    console.log(applications[i])
-    
-    
-    let download = applications[i].download.slice(0, applications[i].download.indexOf('.') + 2);
-    
-    
-    let parsedDownload = Number(download)
+        let parsedDownload = Number(download)
 
-    values.push(parsedDownload)
-    console.log(parsedDownload)
-    console.log(download)
-    
-}
-console.log(values)
+        values.push(parsedDownload)
+        console.log(parsedDownload)
+        console.log(download)
 
-console.log(Math.max(...values))
-const max = Math.max(...values)
-console.log("Consumindo mais >>>>>>")
-if(applications[values.indexOf(max)] != undefined)
-  console.log(applications[values.indexOf(max)].name)
+      }
+      console.log(values)
 
-//console.log(applications[0])
+      console.log(Math.max(...values))
+      const max = Math.max(...values)
+      console.log("Consumindo mais >>>>>>")
+      if (applications[values.indexOf(max)] != undefined)
+        console.log(applications[values.indexOf(max)].name)
+
+      //console.log(applications[0])
 
 
 
 
 
 
-    });  
+    });
 
   }, 5000) //Application needs this delay in order to python execute
 
