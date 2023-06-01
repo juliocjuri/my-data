fe52e2b7e9e81df211e3ec470d92fe99152579a7
Júlio Juriolli <juliocesarjuriolli@gmail.com>
2023-06-01 17:31:46 -0300

    fix: fixed the background bug and removed sidebar for a while

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import HomePage from './pages/Home'
 import Reports from './pages/Reports'
 function App() {
   return (
-    <>
+    <div className='container-content'>
     <HomePage />
     <Reports />
-    </>
+    </div>
   )
 }
 
